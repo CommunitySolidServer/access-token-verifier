0ee900b9b48bd2c863fd59bc4495546c0b188d7e
Matthieu Bosquet <matthieu@cognithive.com>
2021-07-13 11:34:27 +0100

    Getting rid of husky mess

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   "**/*.{js,jsx,ts,tsx}": (filenames) =>
     filenames.map((filename) => `eslint --fix '${filename}'`),
-  "**/*.{css,md,mdx}": (filenames) =>
+  "**/*.{css,html,json,md,mdx}": (filenames) =>
     filenames.map((filename) => `prettier --write '${filename}'`),
 };
