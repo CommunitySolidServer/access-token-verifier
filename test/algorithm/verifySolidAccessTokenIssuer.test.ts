13011c792791635faee8b5b6f467c98ef39cfea5
Matthieu Bosquet <matthieu@cognithive.com>
2021-11-25 21:24:59 +0000

    Add a test for multiple issuers

--- test/algorithm/verifySolidAccessTokenIssuer.test.ts
+++ test/algorithm/verifySolidAccessTokenIssuer.test.ts
@@ -11,6 +11,19 @@ describe("verifySolidAccessTokenIssuer()", () => {
     }).not.toThrow();
   });
 
+  it("doesn't throw when the issuer is listed among other", () => {
+    expect(() => {
+      verifySolidAccessTokenIssuer(
+        [
+          "https://example.other.issuer.com",
+          "https://example.issuer.com",
+          "https://example.issuer.org",
+        ],
+        "https://example.issuer.com"
+      );
+    }).not.toThrow();
+  });
+
   it("throws when the issuer is not listed", () => {
     expect(() => {
       verifySolidAccessTokenIssuer(
