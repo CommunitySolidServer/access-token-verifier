111414a8690722c3c36ca672c2ca90b92c29541b
Matthieu Bosquet <matthieu@cognithive.com>
2021-12-05 11:50:43 +0000

    Clarify the number of JWT segments validation

--- src/algorithm/parseSolidAuthorizationHeader.ts
+++ src/algorithm/parseSolidAuthorizationHeader.ts
@@ -5,9 +5,12 @@ import { verifyAuthenticationScheme } from "./verifyAuthenticationScheme";
 /**
  * Parse an authorization header used in Solid
  *
- * Verify the authorization header uses a known authentication scheme;
- * verify the token's structure and return an object representing the JWT
- * structure and the authentication scheme.
+ * Verify that the authorization header uses a known authentication scheme and
+ * verify that the token is composed of three segments as we currently only
+ * support JWSs.
+ *
+ * > If the object is using the JWS Compact Serialization or the JWE Compact Serialization, the number of base64url-encoded segments separated by period ('.') characters differs for JWSs and JWEs. JWSs have three segments separated by two period ('.') characters. JWEs have five segments separated by four period ('.') characters.
+ * > -- https://datatracker.ietf.org/doc/html/rfc7516#section-9
  *
  * @param authorizationHeader The authorization header used for the request.
  * @return {SolidJwt} A representation of the authorization header.
