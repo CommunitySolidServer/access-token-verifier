f4a9863d97d522c268305f2446f678ac3ea95d58
Matthieu Bosquet <matthieu@cognithive.com>
2021-12-09 19:37:22 +0000

    Refactor test settings

--- .jestrc.js
+++ .jestrc.js
@@ -1,10 +1,10 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testRegex: "/test/.*\\.test\\.ts$",
+  testRegex: ".*\\.test\\.ts$",
   testRunner: "jest-circus/runner",
-  coverageDirectory: "./coverage/",
   collectCoverage: true,
+  coverageReporters: ["text"],
   coverageThreshold: {
     global: {
       branches: 100,
