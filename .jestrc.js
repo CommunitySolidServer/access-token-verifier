ff1929aa36d211caf0a5a9cc75bffeeaa3fca16f
Matthieu Bosquet <matthieu@cognithive.com>
2021-12-20 17:34:47 +0000

    Remove config now default in Jest 27

--- .jestrc.js
+++ .jestrc.js
@@ -1,7 +1,5 @@
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "node",
-  testRunner: "jest-circus/runner",
   collectCoverage: true,
   coverageReporters: ["text"],
   coverageThreshold: {
