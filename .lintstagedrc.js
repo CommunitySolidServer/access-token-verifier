87f1c3e0e7788faee40a6b256c12bb4cdf43fe30
Matthieu Bosquet <matthieu@cognithive.com>
2022-01-10 23:50:56 +0000

    Remove lintstaged

--- .lintstagedrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  "**/*.{js,jsx,ts,tsx}": (filenames) =>
-    filenames.map((filename) => `eslint --fix '${filename}'`),
-  "**/*.{css,html,json,md,mdx,yml}": (filenames) =>
-    filenames.map((filename) => `prettier --write '${filename}'`),
-};
